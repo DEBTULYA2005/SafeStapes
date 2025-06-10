c3a7d16afb076283919ffbb69f911d8c3b410713
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-06-10 15:32:01 +0530

    there is a problem in static

--- Project/settings.py
+++ Project/settings.py
@@ -174,9 +174,11 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+ 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic puts files
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic puts files
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Enable compression and caching (better performance)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
