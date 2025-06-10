732e5df2db0cd1bef37797e94de74a17d6ff5f87
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-06-10 15:22:45 +0530

    currect email .env configuration

--- Project/settings.py
+++ Project/settings.py
@@ -203,8 +203,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email Configuration
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv.int('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv.bool('EMAIL_USE_TLS')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
