de2fe1dfbc3c91f6c1d939976b6c5cffac4a9b5a
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-06-15 23:28:56 +0530

    Add policy and terms

--- Project/settings.py
+++ Project/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False    # change True for local use
 
 CSRF_TRUSTED_ORIGINS = [
     'https://safestapes.onrender.com' 
@@ -65,8 +65,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
 
-    'cloudinary',
-    'cloudinary_storage',
+    'cloudinary',           # Cloudinary for image storage
+    'cloudinary_storage',   # Cloudinary storage backend
 
 ]
 
